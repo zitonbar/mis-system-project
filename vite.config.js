d07ca45526768f53e25f53548a0391a281d2fcdc
ztionbar <ztion1990@gmail.com>
2024-02-04 17:25:22 +0800

    first commit

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/mis-system-project/',
+  base: '/pets/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
